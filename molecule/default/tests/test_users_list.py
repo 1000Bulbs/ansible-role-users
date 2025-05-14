1c4b74871bf6603a75527767d86df025af48769e
Fernando Alemán <fernandoaleman@mac.com>
2025-05-14 18:34:52 -0500

    Add ssh keys to users authorized keys file

--- molecule/default/tests/test_users_list.py
+++ molecule/default/tests/test_users_list.py
@@ -28,3 +28,36 @@ def test_user_exists_and_configured(host):
 
         expected_shell = user.get("shell", users_shell)
         assert u.shell == expected_shell
+
+
+def test_ssh_directory_exists(host):
+    for user in users_list:
+        if "ssh_keys" not in user:
+            continue
+
+        f = host.file(f"/home/{user['username']}/.ssh")
+
+        assert f.exists
+        assert f.is_directory
+        assert f.user == user["username"]
+        assert f.group == user["username"]
+        assert f.mode == 0o700
+
+
+def test_authorized_keys_file_exists_and_contents(host):
+    for user in users_list:
+        if "ssh_keys" not in user:
+            continue
+
+        path = f"/home/{user['username']}/.ssh/authorized_keys"
+
+        f = host.file(path)
+
+        assert f.exists
+        assert f.user == user["username"]
+        assert f.group == user["username"]
+        assert f.mode == 0o600
+
+        content = f.content_string.strip().splitlines()
+        for key in user["ssh_keys"]:
+            assert key in content
